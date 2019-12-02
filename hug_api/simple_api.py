3075a6ff3dd95cf7ae66f23ad855a536a3d2bc6d
Jake O'Rielly <jake.orielly@gmail.com>
2019-12-02 03:57:52 +0000

    Basic login api using hashed and salted passwords

--- hug_api/simple_api.py
+++ hug_api/simple_api.py
@@ -2,31 +2,57 @@ import hug
 import mysql.connector
 from hug_middleware_cors import CORSMiddleware
 import config
+from hash_pass import hash_password, verify_password
 
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api))
 
-@hug.get('/is_alive')
+@hug.get('/is-alive')
 def is_alive():
     return {'result':'API is alive!'}
 
-@hug.get(examples='name=Jake')
-@hug.get('/user_exists')
-def show_users(name: hug.types.text):
-    mydb = mysql.connector.connect(
+@hug.post('/set-password',examples='user=Jake&password=1234')
+def set_password(user: hug.types.text, password: hug.types.text):
+    if not user_exists(user):
+        return {'Error':'User ' + user + ' does not exist'}
+    query = 'UPDATE fake_users SET Password = \'' + hash_password(password) + '\' WHERE UserName = \'' + user + '\''
+    
+    execute_query(query,commit=True)
+    
+    return {'message':'Password updated for user ' + user + '.'}
+
+@hug.post('/login',examples='user=Jake&password=1234')
+def login(user: hug.types.text, password: hug.types.text):
+    password_query = 'SELECT Password from fake_users WHERE UserName="' + user + '"'
+    stored_password = execute_query(password_query,result=True)
+    stored_password = stored_password[0][0]
+    return {'message':verify_password(stored_password,password)}
+
+def user_exists(name):
+    query = 'SELECT * FROM fake_users WHERE UserName="' + name + '"'
+    result = execute_query(query,result=True)
+
+    if (len(result) == 0):
+        return False
+    else:
+        return True
+
+def execute_query(query,commit=False,result=False):
+    db = new_db_connection()
+    cursor = db.cursor()
+    cursor.execute(query)
+    if commit:
+        db.commit()
+    if result:
+        return cursor.fetchall()
+
+def new_db_connection():
+    db = mysql.connector.connect(
         host="localhost",
         user=config.username,
         passwd=config.password,
         database=config.db
     )
 
-    cursor = mydb.cursor()
-
-    cursor.execute('SELECT * FROM fake_users WHERE UserName="' + name + '"')
-
-    result = cursor.fetchall()
-
-    if (len(result) == 0):
-        return {'message':'User ' + name + ' does not exist.'}
-    else:
-        return {'message':'User ' + name + ' exists.'}
+    return db
+    
