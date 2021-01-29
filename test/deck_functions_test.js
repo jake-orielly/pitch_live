fde9d54b72992dc6f0aec579ad57820d238a31eb
Jake O'Rielly <jake.orielly@gmail.com>
2021-01-29 01:40:23 +0000

    Broke tests into separate files

---
+++ test/deck_functions_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const deckFunctions = require('../socket-server/deck_functions.js');
+
+describe('Deck Functions', function() {
+    describe('shuffle', function() {
+      it('check that we\'re getting 52 different cards', function() {
+        let deck = deckFunctions.shuffle();
+        let found = {};
+        let key;
+        assert(deck.length, 52);
+        for (let i of deck) {
+          key = `${i.num}-${i.suit}`;
+          assert.strictEqual(typeof(found[key]), "undefined");
+          found[key] = true;
+        }
+      });
+    });
+  });
--- test/game_function_tests.js
+++ test/game_function_tests.js
@@ -1,22 +1,5 @@
 const assert = require('assert');
-const deckFunctions = require('/home/jake/pitch-live/socket-server/deck_functions.js');
-const gameFunctions = require('/home/jake/pitch-live/socket-server/game_functions.js');
-
-describe('Deck Functions', function() {
-  describe('shuffle()', function() {
-    it('check that we\'re getting 52 different cards', function() {
-      let deck = deckFunctions.shuffle();
-      let found = {};
-      let key;
-      assert(deck.length, 52);
-      for (let i of deck) {
-        key = `${i.num}-${i.suit}`;
-        assert.strictEqual(typeof(found[key]), "undefined");
-        found[key] = true;
-      }
-    });
-  });
-});
+const gameFunctions = require('../socket-server/game_functions.js');
 
 describe('Game Functions', function() {
   describe('point counting', function() {
