39a286e410546820f6dd3b2a58490adac7547b0c
Jake O'Rielly <jake.orielly@gmail.com>
2021-02-03 02:45:59 +0000

    Moved all game functions into lobby class

--- socket-server/index.js
+++ socket-server/index.js
@@ -12,13 +12,8 @@ const io = require('socket.io')(http, {
 });
 const port = 3000;
 
-var gameStartCountdown;
-
 const htmlPath = path.join(__dirname, 'public');
 
-const constants = require('./constants.js');
-const deckFunctions = require('./deck_functions.js');
-const gameFunctions = require('./game_functions.js');
 const Lobby = require('./lobby_class.js')
 
 app.use(express.static(htmlPath));
@@ -26,11 +21,7 @@ http.listen(port, function () {
   console.log('listening on *:' + port);
 });
 
-var users = [];
-var score = [0, 0];
-var gameOver = false;
-var currPlayerNum, currBid, currPlayer, trumpSuit, leadSuit, currTrick, lastDealer;
-var lobbies = [new Lobby()];
+var lobbies = [new Lobby(io)];
 
 console.log(lobbies[0].id)
 
@@ -38,7 +29,6 @@ io.on('connection', function (socket) {
   socket.on('chat', function (msg) {
     io.emit('chat', msg);
   });
-  socket.on('bid', recieveBid);
 
   socket.on('joinLobby', function(id) {
     let lobby = lobbies.filter(
@@ -48,25 +38,29 @@ io.on('connection', function (socket) {
       socket.emit('joinFailed');
     else {
       socket.emit('joinSucceeded');
-      socket.lobby = lobby
+      socket.lobby = lobby;
+      socket.on('bid', (data) => {
+        socket.lobby.recieveBid(data)
+      });
     }
   });
 
   socket.on('usernameSubmission', function (data) {
+    let lobby = socket.lobby;
     let newUser = { 
       username: data['usernameSubmission'], 
       ready: false, 
       socket: socket,
-      teamNum: users.length % 2,
+      teamNum: lobby.getUsers().length % 2,
       cards: 0
     };
     let options;
     let partOfSpeech;
-    users.push(newUser);
+    lobby.addUser(newUser);
     socket.broadcast.emit('chat', `${data['usernameSubmission']} has joined`);
-    partOfSpeech = (parseInt((users.length - 1 ) / 2) == 1 ? "nouns" : "adjectives");
+    partOfSpeech = (parseInt((lobby.getUsers().length - 1 ) / 2) == 1 ? "nouns" : "adjectives");
     teammatePartOfSpeech = (partOfSpeech == "nouns" ? "adjectives" : "nouns");
-    options = socket.lobby.teamWords[(users.length - 1) % 2][partOfSpeech];
+    options = lobby.getTeamWords((lobby.getUsers().length - 1) % 2, partOfSpeech);
     socket.emit('callStoreMutation', {
       mutation:'setTeamWordOptions', 
       val: {
@@ -74,262 +68,64 @@ io.on('connection', function (socket) {
         options
       }
     });
-    callStoreMutation('setTeamNames', [socket.lobby.teams[0].name, socket.lobby.teams[1].name]);
-    sendUpdatedUsers();
+    callStoreMutation('setTeamNames', [lobby.getTeams(0).name, lobby.getTeams(1).name]);
+    lobby.sendUpdatedUsers();
   });
 
   socket.on('disconnect', function () {
     let pos;
-    for (let i = 0; i < users.length; i++)
-      if (users[i].socket.id == socket.id)
+    for (let i = 0; i < socket.lobby.getUsers().length; i++)
+      if (socket.lobby.getUser(i).socket.id == socket.id)
         pos = i;
-    if (users[pos]) {
-      io.emit('chat', `${users[pos].username} has left`);
-      users.splice(pos, 1);
-      sendUpdatedUsers();
+    if (socket.lobby.getUser(pos)) {
+      io.emit('chat', `${socket.lobby.getUser(pos).username} has left`);
+      socket.lobby.removeUser(pos);
+      socket.lobby.sendUpdatedUsers();
     }
   });
 
   socket.on('ready', function (ready) {
     //let count = 5;
     let count = 1;
+    const users = socket.lobby.getUsers();
     users.filter(user => user.socket == socket)[0].ready = ready;
-    sendUpdatedUsers();
+    socket.lobby.sendUpdatedUsers();
     if (count && !ready) {
-      clearInterval(gameStartCountdown);
+      clearInterval(socket.lobby.getGameStartCountdown);
       setProp('gameStarting', 0);
     }
     else if (users.filter(user => !user.ready).length == 0 && users.length == 4) {
       setProp('gameStarting', count);
-      gameStartCountdown = setInterval(function () {
+      socket.lobby.gameStartCountdown = setInterval(function () {
         count--;
         setProp('gameStarting', count);
         if (count == 0) {
-          clearInterval(gameStartCountdown);
+          clearInterval(socket.lobby.gameStartCountdown);
           callStoreMutation('setTeamNames', [
-            socket.lobby.teams[0].name, 
-            socket.lobby.teams[1].name
+            socket.lobby.getTeams(0).name, 
+            socket.lobby.getTeams(1).name
           ]);
           setProp('gameStage', 'playing')
-          setTimeout(dealCards, 500);
+          setTimeout(() => {
+            socket.lobby.dealCards()
+          }, 500);
         }
       }, 1000);
     }
   });
 
   socket.on('play', function (card) {
-    let winning;
+    let currPlayer = socket.lobby.getCurrPlayer();
     sendChat(`${currPlayer.username} played the ${card.num} of ${card.suit}`);
-    currPlayer.cards--;
-    sendUpdatedUsers();
+    socket.lobby.sendUpdatedUsers();
     socket.broadcast.emit('played', { card: card, id: currPlayer.socket.id });
-    currTrick.push({ user: currPlayer, card: card });
-    if (!trumpSuit) {
-      trumpSuit = card.suit
-      callStoreMutation('setTrumpSuit', card.suit)
-    }
-    if (!leadSuit) {
-      leadSuit = card.suit
-      callStoreMutation('setLeadSuit', card.suit)
-    };
-    winning = gameFunctions.evalWinner(currTrick, trumpSuit, leadSuit);
-    callStoreMutation('setLeader', winning.user.username)
-    if (currPlayerNum == users.length - 1) {
-      awardWinner(winning, socket.lobby);
-    }
-    else
-      nextTrick();
+    socket.lobby.playCard(card);
   });
 
   socket.on('selectTeamWord', function(data) {
-    let position = (data.partOfSpeech == 'adjectives' ? 0 : 1);
-    socket.lobby.teams[data.teamNum].name[position] = data.val;
-    callStoreMutation('setTeamNames', [
-      socket.lobby.teams[0].name, 
-      socket.lobby.teams[1].name
-    ]);
+    socket.lobby.setTeamWord(data);
   });
 });
-
-function sendUpdatedUsers() {
-  let simpleUsers = [];
-  users.forEach(function (user) {
-    simpleUsers.push({ 
-      username: user.username,
-      ready: user.ready, 
-      team: user.teamNum, 
-      id:user.socket.id,
-      cards: user.cards 
-    });
-  });
-  for (let i in users) {
-    i = parseInt(i);
-    ordered = rotateArray(simpleUsers, ((i + 1) % simpleUsers.length));
-    users[i].socket.emit('callStoreMutation', {
-      mutation: 'setUsers',
-      val: ordered
-    });
-  }
-}
-
-function awardWinner(winning, lobby) {
-  sendChat(`${winning.user.username} takes it with the ${winning.card.num} of ${winning.card.suit}`)
-  for (let i = 0; i < currTrick.length; i++)
-    lobby.teams[winning.user.teamNum].cards.push(currTrick[i].card)
-  setTimeout(function () { trickReset(winning, lobby); }, 1500);
-};
-
-function trickReset(winner, lobby) {
-  // Rotate users array until winner is in 0th position
-  while (users[0].socket.id != winner.user.socket.id)
-    users.unshift(users.pop())
-  winner.user.socket.broadcast.emit('chat', `${winner.user.username} has the lead`);
-  winner.user.socket.emit('chat', 'Your lead')
-
-  // It will get incremented by nextTrick
-  currPlayerNum = -1;
-
-  callStoreMutation('setLeadSuit', undefined)
-  leadSuit = undefined;
-  currTrick = [];
-
-  io.sockets.emit('newTrick', '');
-  callStoreMutation('setLeader', '')
-  if (lobby.teams[0].cards.length + lobby.teams[1].cards.length == users.length * 6) {
-    nextHand(lobby);
-  }
-  else
-    nextTrick();
-}
-
-function assignPoints() {
-  let biddingTeam;
-  for (let i in users)
-    if (users[i].socket.id == currBid.player.socket.id)
-      biddingTeam = users[i].teamNum;
-
-  if (lobby.teams[biddingTeam].points.length < currBid.amount)
-    score[biddingTeam] -= currBid.amount;
-  else
-    score[biddingTeam] += lobby.teams[biddingTeam].points.length;
-  score[(biddingTeam + 1) % 2] += lobby.teams[(biddingTeam + 1) % 2].points.length;
-
-  // If both teams cross 11 in the same round, the bidding team wins
-  if (score[biddingTeam] >= constants.scoreToWin) {
-    endGame(lobby.teams[biddingTeam])
-  }
-  else if (score[(biddingTeam + 1) % 2] >= constants.scoreToWin) {
-    endGame(lobby.teams[(biddingTeam + 1) % 2]);
-  }
-};
-
-function endGame(team) {
-  callStoreMutation('gameOver', team.name);
-  gameOver = true;
-}
-
-function dealCards() {
-  deck = deckFunctions.shuffle()
-  callStoreMutation('resetDeck');
-  callStoreMutation('setDealer', false);
-  if (lastDealer)
-    users = rotateArray(users, (users.indexOf(lastDealer) + 2) % 4)
-  dealer = users[users.length - 1];
-  lastDealer = dealer;
-  dealer.socket.broadcast.emit('chat', `${dealer.username} is dealer`)
-  dealer.socket.emit('chat', 'You are the dealer')
-  dealer.socket.emit('callStoreMutation', {
-    mutation: 'setDealer',
-    val: true
-  });
-
-  let hand;
-  for (var i = 0; i < users.length; i++) {
-    hand = []
-    for (var j = 0; j < 6; j++)
-      hand.push(deck.pop())
-    users[i].socket.emit('deal', hand)
-  }
-
-  for (let user of users)
-    user.cards = 6;
-  sendUpdatedUsers();
-
-  currBid = { player: '', amount: 'pass' }
-  currPlayerNum = 0;
-  currPlayer = users[currPlayerNum];
-  nextBidder();
-}
-
-function nextBidder() {
-  let addon = '';
-  if (currBid.player)
-    addon = `, ${currBid.player.username} has it for ${currBid.amount}`
-  currPlayer.socket.broadcast.emit('status', `Waiting for ${currPlayer.username} to choose a bid${addon}.`);
-  currPlayer.socket.emit('callStoreMutation', {
-    mutation: 'setCurrBid',
-    val: currBid.amount
-  });
-  currPlayer.socket.emit('setProp', {
-    prop: 'bidding',
-    val: true
-  });
-  currPlayer.socket.emit('status', 'Your bid' + addon);
-}
-
-function recieveBid(data) {
-  if (data != 'pass') {
-    // Any bid that isn't pass will be greater than than the current bid
-    currBid = { player: currPlayer, amount: data }
-    currPlayer.socket.broadcast.emit('chat', `${currPlayer.username} bid ${data}`)
-  }
-  else
-    currPlayer.socket.broadcast.emit('chat', `${currPlayer.username} passed`)
-  currPlayer.socket.emit('setProp', {
-    prop: 'bidding',
-    val: false
-  });
-  currPlayerNum++;
-
-  // If we've had our last bid
-  if (currPlayerNum == users.length)
-    setUpHand();
-  else {
-    currPlayer = users[currPlayerNum];
-    nextBidder();
-  }
-}
-
-function setUpHand() {
-  // Case where dealer gets bid
-  if (!currBid.player) {
-    currPlayerNum = users.length - 1;
-    currPlayer = users[currPlayerNum];
-    currBid = { player: currPlayer.username, amount: 2 }
-  }
-  sendChat(`${currBid.player.username} has it for ${currBid.amount}`);
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].socket.id == currBid.player.socket.id) {
-      currPlayerNum = i;
-      break;
-    }
-  }
-
-  users = rotateArray(users, currPlayerNum);
-  callStoreMutation('setLeadSuit', undefined)
-  callStoreMutation('setTrumpSuit', '')
-  trumpSuit = undefined;
-  leadSuit = undefined;
-  currTrick = [];
-  // Compensating for the increment at start of nextTrick
-  currPlayerNum = -1;
-  nextTrick();
-}
-
-function printPoints(team) {
-  let pointText = team.points.join(", ");
-  return (pointText ? pointText : "nothing")
-}
 
 function setProp(prop, val) {
   io.sockets.emit('setProp', {
@@ -345,34 +141,4 @@ function callStoreMutation(mutation, val) {
 
 function sendChat(msg) {
   io.sockets.emit('chat', msg);
-}
-
-function rotateArray(arr, num) {
-  arr = arr.slice(num, arr.length).concat(arr.slice(0, num))
-  return arr;
-}
-
-function nextTrick() {
-  currPlayerNum = (currPlayerNum + 1) % users.length;
-  currPlayer = users[currPlayerNum]
-  currPlayer.socket.broadcast.emit('status', `Waiting for ${currPlayer.username} to make a play`)
-  currPlayer.socket.emit('status', 'Waiting for you to make a play')
-  currPlayer.socket.emit('setProp', {
-    prop: 'currPlay',
-    val: true
-  });
-}
-
-function nextHand(lobby) {
-  const teamPoints = gameFunctions.countPoints([lobby.teams[0].cards, lobby.teams[1].cards], trumpSuit, currBid);  
-  lobby.teams[0].points = teamPoints[0];
-  lobby.teams[1].points = teamPoints[1];
-  assignPoints(lobby);
-  sendChat(`${lobby.teams[0].name.join(" ")} won ${printPoints(lobby.teams[0])}`)
-  sendChat(`${lobby.teams[1].name.join(" ")} won ${printPoints(lobby.teams[1])}`)
-  setProp('score', score);
-  if (!gameOver) {
-    dealCards();
-    lobby.teams = [{ name: lobby.teams[0].name, cards: [], points: [] }, { name: lobby.teams[1].name, cards: [], points: [] }];
-  }
 }
--- socket-server/lobby_class.js
+++ socket-server/lobby_class.js
@@ -1,25 +1,62 @@
+const constants = require('./constants.js');
+const deckFunctions = require('./deck_functions.js');
 const teamNameWords = require('./team_name_words.js');
+const gameFunctions = require('./game_functions.js');
 
 class Lobby {
-    constructor() {
-        this.id = makeid(4);
+    constructor(io) {
+        this.io = io;
+        this.id = this.makeid(4);
+        this.users = [];
         this.teams = [{ name: [], cards: [], points: [] }, { name: [], cards: [], points: [] }];
         this.teamWords = this.generateTeamWords();
+        this.score = [0, 0]
+        this.gameStartCountdown = 0;
+        this.gameOver = false;
+        this.currPlayerNum = undefined;
+        this.currPlayer = undefined;
+        this.trumpSuit = undefined;
+        this.leadSuit = undefined;
+        this.currBid = undefined;
+        this.currTrick = undefined;
+        this.lastDealer = undefined;
+    }
+
+    sendUpdatedUsers() {
+        let simpleUsers = [];
+        let ordered;
+        this.users.forEach(function (user) {
+            simpleUsers.push({
+                username: user.username,
+                ready: user.ready,
+                team: user.teamNum,
+                id: user.socket.id,
+                cards: user.cards
+            });
+        });
+        for (let i in this.users) {
+            i = parseInt(i);
+            ordered = this.rotateArray(simpleUsers, ((i + 1) % simpleUsers.length));
+            this.users[i].socket.emit('callStoreMutation', {
+                mutation: 'setUsers',
+                val: ordered
+            });
+        }
     }
 
     generateTeamWords() {
         let adjectives = teamNameWords.adjectives.slice();
         let nouns = teamNameWords.nouns.slice();
         const numOptions = 3;
-    
-        shuffleArray(adjectives);
-        shuffleArray(nouns);
-    
+
+        this.shuffleArray(adjectives);
+        this.shuffleArray(nouns);
+
         const team0Adjectives = adjectives.slice(0, numOptions);
         const team0Nouns = nouns.slice(0, numOptions);
         const team1Adjectives = adjectives.slice(numOptions, numOptions * 2);
         const team1Nouns = nouns.slice(numOptions, numOptions * 2);
-    
+
         // Default values in case users don't make a selection
         this.teams[0].name = [team0Adjectives[0], team0Nouns[0]];
         this.teams[1].name = [team1Adjectives[0], team1Nouns[0]];
@@ -28,25 +65,286 @@ class Lobby {
             { adjectives: team1Adjectives, nouns: team1Nouns }
         ]
     };
-}
 
-function shuffleArray(arr) {
-    for (var i = arr.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    playCard(card) {
+        let winning;
+        this.currTrick.push({ user: this.currPlayer, card });
+        this.currPlayer.cards--;
+        if (!this.trumpSuit) {
+            this.trumpSuit = card.suit
+            this.callStoreMutation('setTrumpSuit', card.suit)
+        }
+        if (!this.leadSuit) {
+            this.leadSuit = card.suit
+            this.callStoreMutation('setLeadSuit', card.suit)
+        };
+        winning = gameFunctions.evalWinner(this.currTrick, this.trumpSuit, this.leadSuit);
+        this.callStoreMutation('setLeader', winning.user.username)
+        if (this.currPlayerNum == this.users.length - 1) {
+            this.awardWinner(winning);
+        }
+        else
+            this.nextTrick();
     }
-};
 
-function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    nextTrick() {
+        this.currPlayerNum = (this.currPlayerNum + 1) % this.users.length;
+        this.currPlayer = this.users[this.currPlayerNum];
+        this.currPlayer.socket.broadcast.emit('status', `Waiting for ${this.currPlayer.username} to make a play`)
+        this.currPlayer.socket.emit('status', 'Waiting for you to make a play')
+        this.currPlayer.socket.emit('setProp', {
+            prop: 'currPlay',
+            val: true
+        });
     }
-    return result;
+
+    setTeamWord(data) {
+        let position = (data.partOfSpeech == 'adjectives' ? 0 : 1);
+        this.teams[data.teamNum].name[position] = data.val;
+        this.callStoreMutation('setTeamNames', [
+            this.teams[0].name,
+            this.teams[1].name
+        ]);
+    }
+
+    awardWinner(winning) {
+        this.io.sockets.emit('chat', `${winning.user.username} takes it with the ${winning.card.num} of ${winning.card.suit}`);
+        for (let i = 0; i < this.currTrick.length; i++)
+            this.teams[winning.user.teamNum].cards.push(this.currTrick[i].card)
+        setTimeout(() => { this.trickReset(winning); }, 1500);
+    };
+
+    trickReset(winner) {
+        // Rotate users array until winner is in 0th position
+        while (this.users[0].socket.id != winner.user.socket.id)
+            this.users.unshift(this.users.pop())
+        winner.user.socket.broadcast.emit('chat', `${winner.user.username} has the lead`);
+        winner.user.socket.emit('chat', 'Your lead')
+
+        // It will get incremented by nextTrick
+        this.currPlayerNum = -1;
+
+        this.callStoreMutation('setLeadSuit', undefined)
+        this.leadSuit = undefined;
+        this.currTrick = [];
+
+        this.io.sockets.emit('newTrick', '');
+        this.callStoreMutation('setLeader', '')
+        if (this.teams[0].cards.length + this.teams[1].cards.length == this.users.length * 6) {
+            this.nextHand();
+        }
+        else
+            this.nextTrick();
+    }
+
+    assignPoints() {
+        let biddingTeam;
+        for (let i in this.users)
+            if (this.users[i].socket.id == this.currBid.player.socket.id)
+                biddingTeam = this.users[i].teamNum;
+
+        if (this.teams[biddingTeam].points.length < this.currBid.amount)
+            this.score[biddingTeam] -= this.currBid.amount;
+        else
+            this.score[biddingTeam] += this.teams[biddingTeam].points.length;
+        this.score[(biddingTeam + 1) % 2] += this.teams[(biddingTeam + 1) % 2].points.length;
+
+        // If both teams cross 11 in the same round, the bidding team wins
+        if (this.score[biddingTeam] >= constants.scoreToWin) {
+            this.endGame(this.teams[biddingTeam])
+        }
+        else if (this.score[(biddingTeam + 1) % 2] >= constants.scoreToWin) {
+            this.endGame(this.teams[(biddingTeam + 1) % 2]);
+        }
+    }
+
+    endGame(team) {
+        this.callStoreMutation('gameOver', team.name);
+        this.gameOver = true;
+    }
+
+    dealCards() {
+        let deck;
+        deck = deckFunctions.shuffle();
+        this.callStoreMutation('resetDeck');
+        this.callStoreMutation('setDealer', false);
+        if (this.lastDealer)
+            this.users = this.rotateArray(this.users, (this.users.indexOf(this.lastDealer) + 2) % 4);
+        this.dealer = this.users[this.users.length - 1];
+        this.lastDealer = this.dealer;
+        this.dealer.socket.broadcast.emit('chat', `${this.dealer.username} is dealer`)
+        this.dealer.socket.emit('chat', 'You are the dealer.')
+        this.dealer.socket.emit('callStoreMutation', {
+            mutation: 'setDealer',
+            val: true
+        });
+
+        let hand;
+        for (var i = 0; i < this.users.length; i++) {
+            hand = []
+            for (var j = 0; j < 6; j++)
+                hand.push(deck.pop())
+            this.users[i].socket.emit('deal', hand)
+        };
+
+        this.currBid = { player: '', amount: 'pass' }
+        this.currPlayerNum = 0;
+        this.currPlayer = this.users[this.currPlayerNum];
+
+        for (let user of this.users)
+            user.cards = 6;
+        this.sendUpdatedUsers();
+
+        this.nextBidder();
+    }
+
+    nextBidder() {
+        let addon = '';
+        if (this.currBid.player)
+            addon = `, ${this.currBid.player.username} has it for ${this.currBid.amount}`
+        this.currPlayer.socket.broadcast.emit('status', `Waiting for ${this.currPlayer.username} to choose a bid${addon}.`);
+        this.currPlayer.socket.emit('callStoreMutation', {
+            mutation: 'setCurrBid',
+            val: this.currBid.amount
+        });
+        this.currPlayer.socket.emit('setProp', {
+            prop: 'bidding',
+            val: true
+        });
+        this.currPlayer.socket.emit('status', 'Your bid' + addon);
+    }
+
+    recieveBid(data) {
+        if (data != 'pass') {
+            // Any bid that isn't pass will be greater than than the current bid
+            this.currBid = { player: this.currPlayer, amount: data }
+            this.currPlayer.socket.broadcast.emit('chat', `${this.currPlayer.username} bid ${data}`)
+        }
+        else
+            this.currPlayer.socket.broadcast.emit('chat', `${this.currPlayer.username} passed`)
+        this.currPlayer.socket.emit('setProp', {
+            prop: 'bidding',
+            val: false
+        });
+        this.currPlayerNum++;
+
+        // If we've had our last bid
+        if (this.currPlayerNum == this.users.length)
+            this.setUpHand();
+        else {
+            this.currPlayer = this.users[this.currPlayerNum];
+            this.nextBidder();
+        }
+    }
+
+    setUpHand() {
+        // Case where dealer gets bid
+        if (!this.currBid.player) {
+            this.currPlayerNum = this.users.length - 1;
+            this.currPlayer = this.users[this.currPlayerNum];
+            this.currBid = { player: this.currPlayer.username, amount: 2 }
+        }
+        this.io.sockets.emit('chat', `${this.currBid.player.username} has it for ${this.currBid.amount}`);
+        for (var i = 0; i < this.users.length; i++) {
+            if (this.users[i].socket.id == this.currBid.player.socket.id) {
+                this.currPlayerNum = i;
+                break;
+            }
+        }
+
+        this.users = this.rotateArray(this.users, this.currPlayerNum);
+        this.callStoreMutation('setLeadSuit', undefined)
+        this.callStoreMutation('setTrumpSuit', '')
+        this.trumpSuit = undefined;
+        this.leadSuit = undefined;
+        this.currTrick = [];
+        // Compensating for the increment at start of nextTrick
+        this.currPlayerNum = -1;
+        this.nextTrick();
+    }
+
+    nextHand() {
+        const teamPoints = gameFunctions.countPoints([this.teams[0].cards, this.teams[1].cards], this.trumpSuit, this.currBid);
+        this.teams[0].points = teamPoints[0];
+        this.teams[1].points = teamPoints[1];
+        this.assignPoints();
+        this.io.sockets.emit('chat', `${this.teams[0].name.join(" ")} won ${this.printPoints(this.teams[0])}`);
+        this.io.sockets.emit('chat', `${this.teams[1].name.join(" ")} won ${this.printPoints(this.teams[1])}`);
+        this.io.sockets.emit('setProp', {
+            prop: 'score',
+            val: this.score
+        });
+        if (!this.gameOver) {
+            this.dealCards();
+            this.teams = [{ name: this.teams[0].name, cards: [], points: [] }, { name: this.teams[1].name, cards: [], points: [] }];
+        }
+    }
+
+    callStoreMutation(mutation, val) {
+        this.io.sockets.emit('callStoreMutation', {
+            mutation, val
+        });
+    }
+
+    printPoints(team) {
+        let pointText = team.points.join(", ");
+        return (pointText ? pointText : "nothing")
+    }
+
+    shuffleArray(arr) {
+        for (var i = arr.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    };
+
+    rotateArray(arr, num) {
+        arr = arr.slice(num, arr.length).concat(arr.slice(0, num))
+        return arr;
+    }
+
+    makeid(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    getUsers() {
+        return this.users;
+    }
+
+    getUser(num) {
+        return this.users[num];
+    }
+
+    getTeams(num) {
+        if (num === undefined)
+            return this.teams;
+        else
+            return this.teams[num];
+    }
+
+    addUser(user) {
+        this.users.push(user);
+    }
+
+    removeUser(num) {
+        this.users.splice(num, 1);
+    }
+
+    getCurrPlayer() {
+        return this.currPlayer;
+    }
+
+    getTeamWords(num, partOfSpeech) {
+        return this.teamWords[num][partOfSpeech];
+    }
 }
 
 module.exports = Lobby
